d1f0fc3c75952fa1b73ac11eaa17c8896f4623bf
Sergey Voychenko <s.vouchenko@gmail.com>
2022-05-29 13:31:02 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let elements = document.querySelectorAll('ul > li:nth-child(odd)');
+    for (let elem of elements) {
+        elem.style.background = 'red';
+    }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+    let elements = document.querySelectorAll('ul > li');
+    for (let elem of elements) {
+        if (elem.matches('.likedItem')) {
+            elem.style.background = 'blue';
+        }
+    }
+};
